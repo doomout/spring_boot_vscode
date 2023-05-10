6c913438c716ec215aa868299003181471e3f72b
doomout <doomout1@gmail.com>
2023-05-10 12:04:16 +0900

    삭제 구현

--- src/main/java/com/zerock/spring_boot_ex/controller/BoardController.java
+++ src/main/java/com/zerock/spring_boot_ex/controller/BoardController.java
@@ -80,4 +80,12 @@ public class BoardController {
         redirectAttributes.addAttribute("bno", boardDTO.getBno());
         return "redirect:/board/read";
     }
+
+    @PostMapping("/remove")
+    public String remove(Long bno, RedirectAttributes redirectAttributes) {
+        log.info("remove post...." + bno );
+        boardService.remove(bno);
+        redirectAttributes.addFlashAttribute("result", "removed");
+        return "redirect:/board/read";
+    }
 }
